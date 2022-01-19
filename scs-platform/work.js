3370dc0e2dff2a9510e582e73b70affcc1ffa1bd
elvish7 <elvish7@gmail.com>
2022-01-19 16:19:05 +0800

    lookup done

--- scs-platform/work.js
+++ scs-platform/work.js
@@ -21,17 +21,25 @@ document.getElementById('register').onclick = () => {
     var date = String(document.querySelector("[name=date]").value).split('-').join('');
     var amount = Number(document.querySelector("[name=amount]").value);
     var keys = Array(document.querySelector("[name=keys]").value);
-    contract.methods.register(cName, date, amount, keys).send({ from: account, gas: 4700000}); //value: 50000000000000000
+    contract.methods.register(cName, date, amount, keys).send({ from: account, gas: 4700000}) //value: 50000000000000000
+                                                        .catch((error) => {
+                                                            alert(error['message'])}); 
 }
 
 // Search company's info
 document.getElementById('search-button').onclick = () => {
     var search_cName = String(document.querySelector("[name=search]").value);
-    contract.methods.lookup(search_cName).call().then(function( info ) { 
-        console.log("info: ", info);
-        document.getElementById('getcName').textContent = info[0];
-        document.getElementById('getfDate').textContent = info[1];
-        document.getElementById('getoshare').textContent = info[2];
-        document.getElementById('getcertificates').textContent = info[3];
-    });
+    contract.methods.lookup(search_cName).call()
+                    .then(function( info ) { 
+                        document.getElementById('results').style.display="block";
+                        document.getElementById('noC').style.display = "none";
+                        document.getElementById('getcName').textContent = info[0];
+                        document.getElementById('getfDate').textContent = info[1];
+                        document.getElementById('getoshare').textContent = info[2];
+                        document.getElementById('getcertificates').textContent = info[3];
+                    })
+                    .catch((error) => {
+                        document.getElementById('noC').style.display = "block";
+                        document.getElementById('results').style.display="none";
+                    });
 }
