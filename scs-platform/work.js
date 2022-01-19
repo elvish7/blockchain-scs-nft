72aa0366622f2d434d36776a5c0b9db068940ffa
elvish7 <elvish7@gmail.com>
2022-01-19 11:34:15 +0800

    add lookup table

--- scs-platform/work.js
+++ scs-platform/work.js
@@ -11,17 +11,27 @@ var contract = null;
 
         var accounts = await web3.eth.getAccounts();
         account = await accounts[0];
-
         contract = new web3.eth.Contract(await $.get(ABI), ADDRESS);
-
-        document.getElementById('register').onclick = () => {
-            var cName = String(document.querySelector("[name=cName]").value);
-            var date = String(document.querySelector("[name=date]").value).split('-').join('');
-            var amount = Number(document.querySelector("[name=amount]").value);
-            var keys = Array(document.querySelector("[name=keys]").value);
-            // contract.methods.register(cName, date, amount, keys).send({ from: account, 
-            //                                                         gas: 4700000, value: 50000000000000000});
-            contract.methods.register(cName, date, amount, keys).send({ from: account, gas: 4700000});
-        }
     }
 })();
+
+// Register
+document.getElementById('register').onclick = () => {
+    var cName = String(document.querySelector("[name=cName]").value);
+    var date = String(document.querySelector("[name=date]").value).split('-').join('');
+    var amount = Number(document.querySelector("[name=amount]").value);
+    var keys = Array(document.querySelector("[name=keys]").value);
+    contract.methods.register(cName, date, amount, keys).send({ from: account, gas: 4700000}); //value: 50000000000000000
+}
+
+//search company's info
+document.getElementById('search-button').onclick = () => {
+    var search_cName = String(document.querySelector("[name=search]").value);
+    contract.methods.lookup(search_cName).call().then(function( info ) { 
+        console.log("info: ", info);
+        document.getElementById('getcName').textContent = info[0];
+        document.getElementById('getfDate').textContent = info[1];
+        document.getElementById('getoshare').textContent = info[2];
+        document.getElementById('getcertificates').textContent = info[3];
+    });
+}
